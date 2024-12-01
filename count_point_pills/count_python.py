81150f5df76743a22a35289323c1609e8f19a9d2
Mgil4 <maria.gil.casas@estudiantat.upc.edu>
2024-12-01 10:01:06 +0100

    endpoint pills

--- count_point_pills/count_python.py
+++ count_point_pills/count_python.py
@@ -6,6 +6,88 @@ from skimage.morphology import disk, erosion
 import json
 from datetime import datetime
 
+def count_pills(image_path):
+    pills = cv2.imread(image_path)
+
+    if pills is None:
+        raise FileNotFoundError("The image could not be loaded. Please check the file path.")
+
+    pills_rgb = cv2.cvtColor(pills, cv2.COLOR_BGR2RGB)
+    pills_gray = cv2.cvtColor(pills, cv2.COLOR_BGR2GRAY)
+
+    threshold = 160
+    _, pills_bin = cv2.threshold(pills_gray, threshold, 255, cv2.THRESH_BINARY)
+
+    SE2 = disk(15)
+    pills_eroded = erosion(pills_bin, SE2)
+
+    labeled_image = label(pills_eroded)
+    regions = regionprops(labeled_image)
+
+    num_long = 0
+    num_round = 0
+    indices_long = []
+    indices_round = []
+
+    fig, ax = plt.subplots()
+    ax.imshow(pills_rgb)
+    ax.axis('off')
+
+    for i, region in enumerate(regions):
+        aspect_ratio = region.major_axis_length / region.minor_axis_length
+        eccentricity = region.eccentricity
+        centroid = region.centroid
+
+        if eccentricity > 0.7:
+            num_long += 1
+            indices_long.append(i)
+        else:
+            num_round += 1
+            indices_round.append(i)
+
+    day_of_week = datetime.now().strftime('%A')
+
+    schedule = {
+        'Monday': {'point_round': 2, 'point_long': 1},
+        'Tuesday': {'point_round': 3, 'point_long': 2},
+        'Wednesday': {'point_round': 2, 'point_long': 4},
+        'Thursday': {'point_round': 3, 'point_long': 1},
+        'Friday': {'point_round': 2, 'point_long': 3},
+        'Saturday': {'point_round': 3, 'point_long': 0},
+        'Sunday': {'point_round': 1, 'point_long': 2}
+    }
+
+    point_round = schedule[day_of_week]['point_round']
+    point_long = schedule[day_of_week]['point_long']
+
+    if len(indices_round) >= point_round:
+        for i in range(point_round):
+            centroid = regions[indices_round[i]].centroid
+            minr, minc, maxr, maxc = regions[indices_round[i]].bbox
+            rect = plt.Rectangle((minc, minr), maxc - minc, maxr - minr, edgecolor='blue', linewidth=2, fill=False)
+            ax.add_patch(rect)
+
+    if len(indices_long) >= point_long:
+        for i in range(point_long):
+            centroid = regions[indices_long[i]].centroid
+            minr, minc, maxr, maxc = regions[indices_long[i]].bbox
+            rect = plt.Rectangle((minc, minr), maxc - minc, maxr - minr, edgecolor='red', linewidth=2, fill=False)
+            ax.add_patch(rect)
+
+    annotated_image_path = 'annotated_pill_image.png'
+    plt.savefig(annotated_image_path, bbox_inches='tight', pad_inches=0)
+
+    results = {
+        'total_num_long': num_long,
+        'total_num_round': num_round,
+        'num_long': point_long,
+        'num_round': point_round,
+        'annotated_image_path': annotated_image_path
+    }
+
+    return results
+
+'''
 pills = cv2.imread('./count_point_pills/pill2.jpeg')
 
 if pills is None:
@@ -90,3 +172,4 @@ with open('pill_count_results.json', 'w') as json_file:
     json.dump(results, json_file)
 
 print("Results saved to pill_count_results.json and annotated image saved to annotated_pill_image.png")
+'''
--- tension/prediccions.py
+++ tension/prediccions.py
@@ -4,26 +4,22 @@ from flask_cors import CORS
 from tensorflow.keras.models import load_model
 import numpy as np
 import requests
+from count_point_pills.count_python import count_pills
 
-# Definir la API con Flask
 app = Flask(__name__)
 CORS(app)
 
-# Cargar el modelo entrenado y el scaler
 model = load_model('tension/hypertension_model.h5')
 scaler = joblib.load('tension/scaler.save')
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Obtener los datos del cuerpo de la solicitud que llega del frontend
         data = request.get_json()[0]
         print(data)
 
-        # Verificar si los datos contienen los campos requeridos
         if not all(key in data for key in ['cp', 'thalach', 'exang']):
             return jsonify({'error': 'Faltan datos necesarios'}), 400
 
-        # Preprocesar los datos recibidos
         features = np.array([[
             data['cp'],
             data['thalach'],
@@ -32,7 +28,6 @@ def predict():
         ]])
         features_scaled = scaler.transform(features)
 
-        # Realizar la predicción
         probabilities = model.predict(features_scaled)
         result = float(probabilities[0][0])
 
@@ -42,5 +37,17 @@ def predict():
         print(e)
         return jsonify({'error': str(e)}), 500
     
+@app.route('/countpills', methods=['POST'])
+def count_pills():
+    try:
+        image_path = request.get_json()['image']
+        result = count_pills(image_path)
+
+        return jsonify(result)
+
+    except Exception as e:
+        print(e)
+        return jsonify({'error': str(e)}), 500
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
